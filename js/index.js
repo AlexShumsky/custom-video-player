1dafbf7e5696f7f7d5d17c3861627279a29cdff2
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-12-18 13:48:15 +0300

    feat: add progress bar and its functional

--- js/index.js
+++ js/index.js
@@ -10,10 +10,12 @@ function functionsManager() {
 	playVideoManager();
 	showVideoDurationManager();
 	volumeBarManager();
+	progressBarManager();
 }
 function volumeBarManager() {
 	const soundContainer = document.querySelector('.sound__container');
 	const volumeBarContainer = document.querySelector('.volume__bar-container');
+	const volumeBar = document.querySelector('.volume-bar');
 
 	soundContainer.addEventListener('mouseover', showVolumeBar);
 	soundContainer.addEventListener('mouseout', hideVolumeBar);
@@ -24,9 +26,17 @@ function volumeBarManager() {
 	function hideVolumeBar() {
 		volumeBarContainer.style.width = '0px';
 	}
+
+	volumeBar.addEventListener('mouseup', changeVolume)
+	function changeVolume() {
+		video.volume = volumeBar.value;
+	}
 }
 function playVideoManager() {
+	const nextVideoButton = document.querySelector('.next-b');
+	let counter = 1;
 	playButton.addEventListener('click', playVideo);
+
 	video.addEventListener('ended', changePlayIcon);
 
 	function playVideo() {
@@ -39,7 +49,12 @@ function playVideoManager() {
 			changePlayIcon();
 		}
 	}
-
+	nextVideoButton.addEventListener('click', playNextVideo);
+	function playNextVideo() {
+		(counter == 3) ? (counter = 1) : counter++;
+		video.src = `assets/video/video${counter}.mp4`;
+		changePlayIcon();
+	}
 	function changePlayIcon() {
 		(video.paused) ? (playIcon.src = 'assets/img/svg/play.svg', playIcon.style.filter = 'invert(0%)') :
 			(playIcon.src = 'assets/img/svg/pause.svg', playIcon.style.filter = 'invert(70%)');
@@ -53,8 +68,8 @@ function showVideoDurationManager() {
 	video.addEventListener('timeupdate', showVideoDuration);
 
 	function showVideoDuration() {
-		let durationMinutes = Math.floor(video.duration / 60);
-		let durationSeconds = Math.floor(video.duration - durationMinutes * 60);
+		let durationMinutes = Math.floor(video.duration / 60) || '0';
+		let durationSeconds = Math.floor(video.duration - durationMinutes * 60) || '0';
 		let currentDurationMinutes = Math.floor(video.currentTime / 60);
 		let currentDurationSeconds = Math.floor(video.currentTime - currentDurationMinutes * 60);
 		currentTime.textContent = `${currentDurationMinutes}:${addZero(currentDurationSeconds)}`;
@@ -63,5 +78,23 @@ function showVideoDurationManager() {
 		function addZero(el) {
 			return (+el < 10) ? '0' + el : el;
 		}
+	}
+}
+function progressBarManager() {
+	const videoProgress = document.querySelector('.video-progress');
+	const videoProgressFilled = document.querySelector('.video-progress-filled');
+
+	video.addEventListener('timeupdate', changeProgressBarOnTime);
+	videoProgress.addEventListener('click', changeProgressBarPerClick);
+
+	function changeProgressBarOnTime() {
+		const VideoDurationPersentage = (video.currentTime / video.duration) * 100;
+		videoProgressFilled.style.width = VideoDurationPersentage + '%';
+	}
+	function changeProgressBarPerClick(event) {
+		const progressTime = event.offsetX;
+		const progressPersentage = (progressTime / videoProgress.offsetWidth) * 100;
+		video.currentTime = video.duration * progressPersentage / 100;
+		videoProgressFilled.style.width = progressPersentage + '%';
 	}
 }
